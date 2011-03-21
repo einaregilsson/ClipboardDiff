a98d5eab2fed06f522af0f28b59b6ceff8205774
Einar Egilsson <einar@einaregilsson.com>
2011-03-22 00:14:46 +0100

    Added basic files

---
+++ ClipboardDiffPackage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace EinarEgilsson.ClipboardDiff
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid("b02989c2-1a8e-4f11-81a4-957f1d18db10")]
+    public sealed class ClipboardDiffPackage : Package
+    {
+        public static readonly Guid CommandSetId = new Guid("6f04d587-0360-458b-8501-02b2bc7bb002");
+        public const int ShowSettingsWindowCommandId = 0x100;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (mcs != null)
+            {
+                mcs.AddCommand( new MenuCommand(ShowSettingsWindow, new CommandID(CommandSetId, ShowSettingsWindowCommandId)));
+            }
+        }
+
+        private void ShowSettingsWindow(object sender, EventArgs e)
+        {
+            // Show a Message Box to prove we were here
+            IVsUIShell uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
+            Guid clsid = Guid.Empty;
+            int result;
+            ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                       0,
+                       ref clsid,
+                       "ClipboardDiff",
+                       String.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
+                       String.Empty,
+                       0,
+                       OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                       OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                       OLEMSGICON.OLEMSGICON_INFO,
+                       0,        // false
+                       out result));
+        }
+
+    }
+}
